84054e5558c7ca3d598f8116e2e2c3a0f4126e2b
BrianLusina <lusinabrian@gmail.com>
2016-11-26 17:57:51 +0300

    import dependencies

---
+++ main.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import operator
+import json
+from tabulate import tabulate
+import sys
+from stop_words import get_stop_words
+
+
+# get data from wikipedia
+
+
