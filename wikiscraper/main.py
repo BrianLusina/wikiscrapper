bb62ff9cf43883ffae3edb270ef907719b4d247d
BrianLusina <lusinabrian@gmail.com>
2016-11-26 19:41:55 +0300

    update project structure

--- wikiscraper/main.py
+++ wikiscraper/main.py
@@ -1,75 +1,10 @@
-from bs4 import BeautifulSoup
 import requests
-import re
 import operator
 import json
 from tabulate import tabulate
 import sys
-from stop_words import get_stop_words
-from wikiscraper.constants import WIKI_API_LINK, WIKI_LINK
-
-
-# get the words
-def get_word_list(url):
-    word_list = []
-    # raw data
-    source_code = requests.get(url)
-    # convert to text
-    plain_text = source_code.text
-    # lxml format
-    soup = BeautifulSoup(plain_text, 'lxml')
-
-    # find the words in paragraph tag
-    for text in soup.findAll('p'):
-        if text.text is None:
-            continue
-        # content
-        content = text.text
-        # lowercase and split into an array
-        words = content.lower().split()
-
-        # for each word
-        for word in words:
-            # remove non-chars
-            cleaned_word = clean_word(word)
-            # if there is still something there
-            if len(cleaned_word) > 0:
-                # add it to our word list
-                word_list.append(cleaned_word)
-
-    return word_list
-
-
-# clean word with regex
-def clean_word(word):
-    cleaned_word = re.sub('[^A-Za-z]+', '', word)
-    return cleaned_word
-
-
-def create_frequency_table(word_list):
-    # word count
-    word_count = {}
-    for word in word_list:
-        # index is the word
-        if word in word_count:
-            word_count[word] += 1
-        else:
-            word_count[word] = 1
-
-    return word_count
-
-
-# remove stop words
-def remove_stop_words(frequency_list):
-    stop_words = get_stop_words('en')
-
-    temp_list = []
-    for key, value in frequency_list:
-        if key not in stop_words:
-            temp_list.append([key, value])
-
-    return temp_list
-
+from wikiscraper import constants
+from wikiscraper import get_word_list, remove_stop_words, create_frequency_table
 
 # if the search word is too small, throw error
 if len(sys.argv) < 2:
@@ -86,55 +21,60 @@ else:
     search_mode = False
 
 # create our URL
-url = WIKI_API_LINK + string_query
+url = constants.WIKI_API_LINK + string_query
 
-# try-except block. simple way to deal with exceptions
-# great for HTTP requests
-try:
-    # use requests to retrieve raw data from wiki API URL we
-    # just constructed
-    response = requests.get(url)
 
-    # format that data as a JSON dictionary
-    data = json.loads(response.content.decode("utf-8"))
+def main(url):
+    # great for HTTP requests
+    try:
+        # use requests to retrieve raw data from wiki API URL we
+        # just constructed
+        response = requests.get(url)
 
-    # page title, first option
-    # show this in web browser
-    wikipedia_page_tag = data['query']['search'][0]['title']
+        # format that data as a JSON dictionary
+        data = json.loads(response.content.decode("utf-8"))
 
-    # get actual wiki page based on retrieved title
-    url = WIKI_LINK + wikipedia_page_tag
-    # get list of words from that page
-    page_word_list = get_word_list(url)
-    # create table of word counts, dictionary
-    page_word_count = create_frequency_table(page_word_list)
-    # sort the table by the frequency count
-    sorted_word_frequency_list = sorted(page_word_count.items(), key=operator.itemgetter(1), reverse=True)
-    # remove stop words if the user specified
-    if search_mode:
-        sorted_word_frequency_list = remove_stop_words(sorted_word_frequency_list)
+        # page title, first option
+        # show this in web browser
+        wikipedia_page_tag = data['query']['search'][0]['title']
 
-    # sum the total words to calculate frequencies
-    total_words_sum = 0
-    for key, value in sorted_word_frequency_list:
-        total_words_sum = total_words_sum + value
+        # get actual wiki page based on retrieved title
+        url = constants.WIKI_LINK + wikipedia_page_tag
+        # get list of words from that page
+        page_word_list = get_word_list(url)
+        # create table of word counts, dictionary
+        page_word_count = create_frequency_table(page_word_list)
+        # sort the table by the frequency count
+        sorted_word_frequency_list = sorted(page_word_count.items(), key=operator.itemgetter(1), reverse=True)
+        # remove stop words if the user specified
+        if search_mode:
+            sorted_word_frequency_list = remove_stop_words(sorted_word_frequency_list)
 
-    # just get the top 20 words
-    if len(sorted_word_frequency_list) > 20:
-        sorted_word_frequency_list = sorted_word_frequency_list[:20]
+        # sum the total words to calculate frequencies
+        total_words_sum = 0
+        for key, value in sorted_word_frequency_list:
+            total_words_sum = total_words_sum + value
 
-    # create our final list which contains words, frequency (word count), percentage
-    final_list = []
-    for key, value in sorted_word_frequency_list:
-        percentage_value = float(value * 100) / total_words_sum
-        final_list.append([key, value, round(percentage_value, 4)])
+        # just get the top 20 words
+        if len(sorted_word_frequency_list) > 20:
+            sorted_word_frequency_list = sorted_word_frequency_list[:20]
 
-    # headers before the table
-    print_headers = ['Word', 'Frequency', 'Frequency Percentage']
+        # create our final list which contains words, frequency (word count), percentage
+        final_list = []
+        for key, value in sorted_word_frequency_list:
+            percentage_value = float(value * 100) / total_words_sum
+            final_list.append([key, value, round(percentage_value, 4)])
 
-    # print the table with tabulate
-    print(tabulate(final_list, headers=print_headers, tablefmt='orgtbl'))
+        # headers before the table
+        print_headers = ['Word', 'Frequency', 'Frequency Percentage']
 
-# throw an exception in case it breaks
-except requests.exceptions.Timeout:
-    print("The server didn't respond. Please, try again later.")
+        # print the table with tabulate
+        print(tabulate(final_list, headers=print_headers, tablefmt='orgtbl'))
+
+    # throw an exception in case it breaks
+    except requests.exceptions.Timeout:
+        print("The server didn't respond. Please, try again later.")
+
+
+if __name__ == "__main__":
+    main(url)
